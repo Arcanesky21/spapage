405a48abcfd4a145525c1d538b68e3d7885554c3
Mikarlo Francis <arcanesky23@gmail.com>
2024-04-24 20:29:06 -0500

    navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Home from "./pages/home/home.tsx";
 
 function App() {
 
   return (
-    <>
-
-    </>
+      <>
+          <Home />
+      </>
   )
 }
 
---
+++ src/components/about.tsx
@@ -0,0 +1,10 @@
+export default function About(){
+
+    return(
+        <div>
+            <div>
+                <h1 className="text-[#b0b0b0] text-3xl  font-bold">Front End Developer</h1>
+            </div>
+        </div>
+    )
+}
